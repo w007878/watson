87d02a5faf15b002ed43242bfb09971b11f7a01f
MO Fan <w007878@hotmail.com>
2017-10-26 07:06:14 +0800

    .

--- python/test.py
+++ python/test.py
@@ -8,7 +8,7 @@ T = tf.constant(np.transpose(np.repeat([t], n, axis=0)))
 j = tf.constant(np.array(range(n)), tf.float32)
 
 def Waston():
-    x = tf.placeholder(tf.float32, [n])
+    x = tf.Variable(np.zeros(n), tf.float32)
     f = tf.reduce_sum((j * (T ** (j - 1)) * x[0:29] - ((T ** j) * x[0:29]) ** 2 - 1) ** 2)
     f += x[0] ** 2 + (x[1] - x[0] ** 2 - 1) ** 2
     return f
@@ -16,9 +16,8 @@ def Waston():
 if __name__ == '__main__':
     sess = tf.Session()
 
-    x0 = tf.Variable(np.zeros(n))
     f = Waston()
     train_step = tf.train.AdamOptimizer(1e-4).minimize(f)
     sess.run(tf.globle_variables_initializer())
-    sess.run(train_step, feed_dict = {x:x0})
-    print f.eval(sess, feed_dict={x:x0})
+    sess.run(train_step, feed_dict={})
+    print f.eval(sess, feed_dict={})
