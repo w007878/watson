36ecf7c390c0efb701f93b221827e7ecb37f2d66
snowyjone <w007878@hotmail.com>
2017-10-26 18:23:11 +0800

    finish checking functions

--- python/test.py
+++ python/test.py
@@ -2,28 +2,34 @@ import tensorflow as tf
 import numpy as np
 import scipy
 
-n = input()
-t = np.array(range(1, 30), np.float64) / 29
+n = input("Please input Dim of x ")
+t = np.array(range(1, 30), np.float64) / 29.
 T = tf.constant(np.transpose(np.repeat([t], n, axis=0)))
 j = tf.constant(np.array(range(n)), tf.float64)
 
+print T
+print j
+
 def Waston():
-    x = tf.Variable(np.zeros(n), tf.float64)
+    x = tf.Variable(np.zeros(n), tf.float64, name='x')
     r = tf.reduce_sum(j * (T ** (j - 1)) * x[0:29], 1) - \
-        tf.reduce_sum(((T ** j) * x[0:29]) ** 2, 1) - 1
+        tf.reduce_sum((T ** j) * x[0:29], 1) ** 2 - 1
 #    print ((j * (T ** (j - 1)) * x[0:29] - ((T ** j) * x[0:29]) ** 2 - 1) ** 2).shape
     print r.shape
     f = tf.reduce_sum((r ** 2))
     f += x[0] ** 2 + (x[1] - x[0] ** 2 - 1) ** 2
-    return f
+    return f, x
 
 if __name__ == '__main__':
     sess = tf.Session()
 
-    f = Waston()
-    train_step = tf.train.AdamOptimizer(1e-4).minimize(f)
+    fval, xmin = Waston()
+    train_step = tf.train.GradientDescentOptimizer(0.001, False).minimize(fval)
     sess.run(tf.global_variables_initializer())
-    print f.eval(session=sess, feed_dict={})
+    print fval.eval(session=sess, feed_dict={})
 
-    sess.run(train_step, feed_dict={})
-    print f.eval(session=sess, feed_dict={}), x.eval(session=sess, feed_dict={})
+    for i in xrange(10000):
+        sess.run(train_step, feed_dict={})
+    print fval.eval(session=sess, feed_dict={})
+    print xmin.eval(session=sess)
+    
